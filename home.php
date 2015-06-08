e0dfebc4670ef19cbe0bf3105de7f4586f45b00f
Matthew Shawkat <matthew@hexolab.com>
2015-06-08 22:36:42 +0100

    add author's picture

--- home.php
+++ home.php
@@ -16,14 +16,9 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 
-
-
-
-
-
-
                 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
                 	<header class="entry-header">
+                		<?php echo get_avatar( get_the_author_meta('ID'), 60); ?>
                 		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
                 		<?php if ( 'post' == get_post_type() ) : ?>
