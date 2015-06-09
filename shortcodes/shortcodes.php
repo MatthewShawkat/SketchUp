a58489bce2b79198f7872f4421242be2bbd9a7f7
Matthew Shawkat <matthew@hexolab.com>
2015-06-09 20:47:26 +0100

    social icons

--- shortcodes/shortcodes.php
+++ shortcodes/shortcodes.php
@@ -12,3 +12,4 @@ require get_template_directory() . '/shortcodes/availability/availability.php';
 require get_template_directory() . '/shortcodes/section/section.php';
 require get_template_directory() . '/shortcodes/skill/skill.php';
 require get_template_directory() . '/shortcodes/portfolio/portfolio.php';
+require get_template_directory() . '/shortcodes/social-icon/social-icon.php';
---
+++ shortcodes/social-icon/social-icon.php
@@ -0,0 +1,20 @@
+<?php function getIconHtml ($icon, $url) {
+    $path = "/wp-content/themes/sketchup/assets/images/icons/" . $icon . ".png";
+    ob_start(); ?>
+        <a class="social-icon" href="<?php echo $url ?>"><img class="social-icon_img" src="<?php echo $path ?>"></a>
+<?php
+    return ob_get_clean();
+} ?>
+
+<?php
+
+/**
+ */
+
+function icon_function($attrs, $content) {
+    $icon = $attrs['icon'];
+    $url = $attrs['url'];
+
+	return getIconHtml($icon, $url);
+}
+add_shortcode('social-icon', 'icon_function');
